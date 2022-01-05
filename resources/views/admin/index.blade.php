81a609ffe2fbf723517fe65e4893258e132a802c
Anthony Bacilio Ruiz <bacilio.anthony@gmail.com>
2022-01-05 18:37:31 -0500

    se puede iniciar

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Panel Admin')
+
+@section('content_header')
+  <h1> Panel Admin</h1>
+@stop
+
+@section('content')
+  <p> welcome to the admins pannel </p>
+@stop
+
+@section('css')
+  <link rel="stylesheet" href="/css/admin_custom_css">
+@stop
+
+@section('js')
+  <script>console.log('Hi');</script>
+@stop
